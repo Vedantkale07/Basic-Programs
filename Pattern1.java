53c607340acbbd7486aea730062a147499272b72
vedant Kale <vedantkale0707@gmail.com>
2025-05-23 23:52:00 +0530

    PalindromNUmber is addded

---
+++ Pattern1.java
@@ -0,0 +1,23 @@
+package InterviewPrograms;
+
+public class Pattern1 {
+	
+	public static void main(String[] args) {
+		
+		for(int i=0;i<5;i++)
+		{
+			for(int j=0;i<5;j++)
+			{
+				if(i==2) {
+				System.out.print("$ ");
+				}
+				else
+				{
+					System.out.print("* ");
+				}
+			}
+			System.out.println();
+		}
+	}
+
+}
